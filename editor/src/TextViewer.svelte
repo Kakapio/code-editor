24a57dfd9063036f1490e8eecaee879f90a3be73
Roshan Patel <ovicior@gmail.com>
2024-04-15 21:15:26 -0400

    halfway functioning, need to redo rewrite system.

--- editor/src/TextViewer.svelte
+++ editor/src/TextViewer.svelte
@@ -1,13 +1,13 @@
 <!-- TextEditor.svelte -->
 <script>
+	import { writable } from 'svelte/store';
+
 	export let text = '';
 
 	/**
 	 * @type {{ [x: string]: any; }[]}
 	 */
 	export let rewrites = [];
-
-	let activeTab = 'textarea';
 	export let originalText = '';
 	/**
 	 * @type {string[]}
@@ -16,12 +16,23 @@
 	let originalLines = [];
 	/**
 	 * @type {{value: string; flag: boolean}[]}
+	 * Each index represents the state of the diff after the nth rewrite is applied.
 	 */
-	let beforeLines = [];
+	let beforeFrames = [];
 	/**
 	 * @type {{value: string; flag: boolean}[]}
 	 */
-	let afterLines = [];
+	let afterFrames = [];
+
+	/**
+	 * @type {{ [x: string]: any; }[]}
+	 * These are the finished sets of rows for each step. Essentially rendered data.
+	 * Each steps changes are applied to the output of the previous step.
+	 * This means we can get the state at each point even when recursive rewrites are applied.
+	 */
+	let renderedRewrites = [];
+	let activeTab = 'textarea';
+	let currentStep = writable(0);
 
 	/**
 	 * @param {{ target: { value: string; }; }} event
@@ -35,13 +46,38 @@
 	 */
 	export function createDiff() {
 		originalLines = originalText.split('\n');
-		generateTables();
+		// Map over originalLines and transform each line into a tuple
+		let tuples = originalLines.map((line) => ({ value: line, flag: true }));
+
+		// Until all our rewrites are consumed or we hit 10 rewrites.
+		let timeoutLimit = 0;
+		while (rewrites.length > 0 && timeoutLimit < 10) {
+			if (afterFrames.length == 0) {
+				// First iteration
+				let out = generateFrame(tuples);
+				beforeFrames.push(out.before); // out.before is the beforeList of tuples <string, bool>.
+				afterFrames.push(out.after);
+			} else {
+				console.warn(
+					'Beforeframes has ' +
+						beforeFrames.length +
+						'Afterframes has ' +
+						(afterFrames.length - 1) +
+						' in it'
+				);
+				let out = generateFrame(afterFrames[afterFrames.length - 1]); // Use our newest frame for the next frame.
+				beforeFrames.push(out.before);
+				afterFrames.push(out.after);
+			}
+
+			timeoutLimit++;
+		}
 	}
 
 	export function clear() {
 		text = 'This is where your cleaned up code will be...';
-		beforeLines.length = 0; // Clear our tables.
-		afterLines.length = 0;
+		beforeFrames.length = 0; // Clear our tables.
+		afterFrames.length = 0;
 	}
 
 	/**
@@ -51,20 +87,45 @@
 		activeTab = tabName;
 	}
 
-	function generateTables() {
-		beforeLines.length = 0; // Clear our tables.
-		afterLines.length = 0;
+	// Function to increment currentStep
+	function addStep() {
+		currentStep.update((value) => {
+			if (value < rewrites.length - 1) {
+				return value + 1;
+			}
+			return value;
+		});
+	}
+
+	function subtractStep() {
+		currentStep.update((value) => {
+			if (value > 0) {
+				return value - 1;
+			}
+			return value;
+		});
+	}
+
+	/**
+	 * @param {{value: string, flag: boolean}[]} [inputLines]
+	 */
+	function generateFrame(inputLines) {
+		inputLines = inputLines || []; // Provide a default value if inputLines is undefined
+		let beforeLines = []; // Tables to hold this particular frame.
+		let afterLines = [];
 
 		let currBytes = 0; // the number of bytes we have consumed thus far. Used to track our position.
-		console.log('Rewrites length is ' + rewrites.length);
+		let foundMatchEver = false; // whether we have found any matches during this function call.
+		console.error('Rewrites length is ' + rewrites.length);
 
 		// I make a lot of assumptions here about rewrites being by line, etc...
 		// A rewrite occurring in the middle of a line would break this. But even GitHub doesn't do that.
-		for (let i = 0; i < originalLines.length; i++) {
-			let line = originalLines[i];
+		for (let i = 0; i < inputLines.length; i++) {
+			let line = inputLines[i].value;
+			console.warn('This should be the same line: ' + JSON.stringify(inputLines[i].value));
 			// Count how much white space we have... This is ignored on the backend so we must also do that.
 			let whitespaceCount = line.length - line.trimStart().length;
-			let foundMatch = false; // So we can track whether a match was found outside the inner loop.
+			let foundLineMatch = false; // So we can track whether a match was found for this particular line.
 
 			// Check the bounds of each line against every item in the rewrite array...
 			// This would be more efficient if rewrite array was ordered by start_byte. Could do O(n) rather than (n^2).
@@ -74,45 +135,53 @@
 				let start = currBytes; // Starting byte of this line.
 				let end = start + line.length; // End byte.
 
-				console.log(
-					'line:' +
-						line +
-						'\nstart: ' +
-						start +
-						' start_byte: ' +
-						rewrite.range.start_byte +
-						'\nend: ' +
-						end +
-						' end_byte: ' +
-						rewrite.range.end_byte +
-						'\n first condition: ' +
-						(rewrite.range.start_byte >= start) +
-						' second condition: ' +
-						(rewrite.range.end_byte <= end) +
-						'\ncurrBytes: ' +
-						currBytes +
-						' whitespacecount: ' +
-						whitespaceCount
-				);
+				// console.log(
+				// 	'line:' +
+				// 		line +
+				// 		'\nstart: ' +
+				// 		start +
+				// 		' start_byte: ' +
+				// 		rewrite.range.start_byte +
+				// 		'\nend: ' +
+				// 		end +
+				// 		' end_byte: ' +
+				// 		rewrite.range.end_byte +
+				// 		'\n first condition: ' +
+				// 		(rewrite.range.start_byte >= start) +
+				// 		' second condition: ' +
+				// 		(rewrite.range.end_byte <= end) +
+				// 		'\ncurrBytes: ' +
+				// 		currBytes +
+				// 		' whitespacecount: ' +
+				// 		whitespaceCount
+				// );
 
 				// This particular line has been found in our rewrites...
-				if (rewrite.range.start_byte >= start && rewrite.range.end_byte <= end) {
-					console.warn('Found a diff at line: ', i);
+				// !foundMatchEver is here because once we find a match for a rewrite, we are done. We limit
+				// each diff to one change.
+				if (!foundMatchEver && rewrite.range.start_byte >= start && rewrite.range.end_byte <= end) {
+					console.log("Found a diff at line: " + i + "\npushing before: " + JSON.stringify(rewrite.original)
+					+ "\npushing after: " + JSON.stringify(rewrite.replacement));
+					rewrites = rewrites.filter((_, index) => index !== j); // Remove this used rewrite
 					beforeLines.push({ value: rewrite.original, flag: true });
 					afterLines.push({ value: rewrite.replacement, flag: true });
-					foundMatch = true;
+					foundLineMatch = true;
+					foundMatchEver = true;
 					break; // TODO: Could there be more than one rewrite for a given set of bytes? here we just assume not.
 				}
 			}
 
-			if (!foundMatch) {
-				console.warn('Found nothing at line: ', i);
-				beforeLines.push({ value: originalLines[i], flag: false });
-				afterLines.push({ value: originalLines[i], flag: false });
+			// If we couldn't find a changed line, just use the original.
+			if (!foundLineMatch) {
+				//console.warn('Found nothing at line: ', i);
+				beforeLines.push({ value: inputLines[i].value, flag: false });
+				afterLines.push({ value: inputLines[i].value, flag: false });
 			}
 
-			currBytes += originalLines[i].length + 1; // Plus 1 for the new line we just traversed.
+			currBytes += inputLines[i].value.length + 1; // Plus 1 for the new line we just traversed.
 		}
+
+		return { before: beforeLines, after: afterLines };
 	}
 </script>
 
@@ -121,7 +190,13 @@
 		<button on:click={() => switchTab('textarea')} class:selected={activeTab === 'textarea'}
 			>Normal</button
 		>
-		<button on:click={() => switchTab('table')} class:selected={activeTab === 'table'}>Diff</button>
+		<button on:click={() => switchTab('table')} class:selected={activeTab === 'table'}
+			>Rewrite Stepper</button
+		>
+		{#if rewrites.length > 0 && activeTab === 'table'}
+			<button on:click={subtractStep}>Step Back</button>
+			<button on:click={addStep}>Step Forward</button>
+		{/if}
 	</div>
 	<div class="tab-content">
 		{#if activeTab === 'textarea'}
@@ -130,7 +205,7 @@
 				readonly={true}
 				class:default-text={text === 'This is where your cleaned up code will be...'}
 			></textarea>
-		{:else if activeTab === 'table'}
+		{:else if activeTab === 'table' && beforeFrames.length > 0}
 			<div class="two-column-table">
 				<table>
 					<thead>
@@ -139,7 +214,7 @@
 						</tr>
 					</thead>
 					<tbody>
-						{#each beforeLines as { value, flag }}
+						{#each beforeFrames[$currentStep] as { value, flag }}
 							<tr>
 								<td><pre class:codered={flag}>{value}</pre></td>
 							</tr>
@@ -154,7 +229,7 @@
 						</tr>
 					</thead>
 					<tbody>
-						{#each afterLines as { value, flag }}
+						{#each afterFrames[$currentStep] as { value, flag }}
 							<tr>
 								<td><pre class:codegreen={flag}>{value}</pre></td>
 							</tr>
@@ -189,7 +264,7 @@
 
 	tr {
 		background-color: #191b22;
-		line-height: 0.1; /* Adjust this value as needed */
+		line-height: 0.7; /* Adjust this value as needed */
 	}
 
 	table {
