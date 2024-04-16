ab8f2bbc7590140087922d6b11edb98acd07484c
Roshan Patel <ovicior@gmail.com>
2024-04-15 23:52:26 -0400

    removed tables from diffview. pin.

--- editor/src/DiffView.svelte
+++ editor/src/DiffView.svelte
@@ -8,42 +8,13 @@
 
     export function createDiff() {
         console.warn("okayyy we are making diffs");
-        
+
     }
 
 </script>
 
-<div class="two-column-table">
-    <table>
-        <thead>
-            <tr>
-                <th>Original</th>
-            </tr>
-        </thead>
-        <tbody>
-            {#each beforeFrames as { value, flag }}
-                <tr>
-                    <td><pre class:codered={flag}>{value}</pre></td>
-                </tr>
-            {/each}
-        </tbody>
-    </table>
-
-    <table>
-        <thead>
-            <tr>
-                <th>Replacement</th>
-            </tr>
-        </thead>
-        <tbody>
-            {#each afterFrames as { value, flag }}
-                <tr>
-                    <td><pre class:codegreen={flag}>{value}</pre></td>
-                </tr>
-            {/each}
-        </tbody>
-    </table>
-</div>
+<textarea readonly></textarea>
+<textarea readonly></textarea>
 
 <style>
 textarea {
@@ -56,41 +27,4 @@ textarea {
 		color: white;
         resize: none;
 	}
-
-    td {
-		color: white;
-		font-family: 'Roboto', sans-serif;
-	}
-
-	th {
-		color: rgb(95, 218, 255);
-		font-family: 'Roboto', sans-serif;
-	}
-
-	tr {
-		background-color: #191b22;
-		line-height: 0.7; /* Adjust this value as needed */
-	}
-
-	table {
-		width: 100%;
-		height: 523px;
-		border-spacing: 0; /* Remove gaps between table rows */
-		border: 2px solid #21242d;
-	}
-
-    .codered {
-		line-height: 0.1; /* Adjust this value as needed */
-		background-color: #642121;
-	}
-
-	.codegreen {
-		line-height: 0.1; /* Adjust this value as needed */
-		background-color: #246421;
-	}
-
-    .two-column-table {
-		display: flex;
-	}
-
 </style>
